2141e482288c96cfcd1ca6b9983b33e5c28bc100
Gabriel Sorato <gabrielsorato29@gmail.com>
2021-03-15 15:14:53 -0300

    Updating the combat

--- combat.js
+++ combat.js
@@ -1,20 +1,21 @@
 /*
-
 COMBAT PHASES
 1 - The character roll the attack (20 sided dice)
 2 - The enemy validates the damage (Compare the value of the attack roll with the value of the armor class)
 3 - If the attack value is greater, the character rolls the damage, plus bonus. 
-
 */
 
 
-let dice = Math.floor(Math.random () * 20 + 1);
-let damage = Math.floor(Math.random () * 8 + 1);
-let bonusAttack = 2
+let d20 = Math.floor(Math.random () * 20 + 1);
+let d8 = Math.floor(Math.random () * 8 + 1);
+let bonusRoll = 2
 let armorClass = 14
-let attack = dice + bonusAttack
+let attack = d20 + bonusRoll
+let bonusDamage = 5;
+let damage = d8 + bonusDamage
 
-console.log(`Your attack is: ${attack}`);
+console.log(`Your d20 roll is ${d20}, and your bonus attack is ${bonusRoll}`)
+console.log(`Your total attack is: ${attack}`);
 console.log(`The enemy's armor class is: ${armorClass}`);
 
 let sucess = attack >= armorClass
@@ -29,6 +30,7 @@ if (sucess) {
 }
 
 if (sucess) {
+    console.log(`Your d8 roll is ${d8}, and your bonus damage is ${bonusDamage}`)
     console.log(`Your damage is: ${damage}`)
 };
 
